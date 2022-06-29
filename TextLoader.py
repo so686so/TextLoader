66e1060af580c927ce73aea3eaeb620e79ba677c
SoByungJun <so5063@naver.com>
2022-06-29 13:42:02 +0900

    add prototype TextLoader module & UnitTest file

---
+++ TextLoader.py
@@ -0,0 +1,133 @@
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+# *  TITLE        |  TextLoader Package                                         * #
+# *  VERSION      |  0.0.1                                                      * #
+# *  INFORMATION  |  Load text in any encoding and convert each line to a list  * #
+# *  AUTHOR       |  So Byung Jun (so686so@gmail.com)                           * #
+# *  GIT          |                             * #
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+
+"""
+[ Supported encodings ]
+-----------------------
+    - UTF-8
+    - ASCII
+    - EUC-KR
+    - CP949
+
+    : For KR Language :D
+"""
+
+
+# Required Package
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+# chardet = 4.0.0
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+
+
+# Import Packages and Modules
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+# Standard Library
+# -------------------------------------------------------------------------------
+import  os
+
+# Installed Library
+# -------------------------------------------------------------------------------
+from    chardet     import detect
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+
+
+# TextLoader Class
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+class TextLoader:
+    def __init__(self, initFileName:str=None) -> None:
+        self._targetFile        :str    = None
+        self._originEncoding    :str    = None
+
+        if initFileName:
+            self.setFile(initFileName)
+
+
+    def setFile(self, fileName:str):
+        if os.path.isfile(fileName) is False:
+            raise Exception(f'[TextLoader/setFile] \'{fileName}\' is not exist.')
+        
+        self._targetFile        = fileName
+        self._originEncoding    = self._getFileEncoding()
+
+        return self
+
+
+    def _getFileEncoding(self, fileName:str=None) -> str:
+        if not self._targetFile and not fileName:
+            raise Exception(f'[TextLoader/getFileEncoding] File Not Registered.')
+
+        if fileName:
+            pass
+        else:
+            fileName = self._targetFile
+
+        if os.path.isfile(fileName) is False:
+            raise Exception(f'[TextLoader/getFileEncoding] \'{fileName}\' is not exist.')
+
+        try:
+        # CP949, EUC-KR, ASCII
+            with open(fileName, 'r') as rf:
+                file_data = rf.readline()
+
+            encFormat:str = detect(file_data.encode())['encoding']
+            return encFormat.upper()
+
+        # UTF-8 cannot read by readline() func
+        except Exception:
+            return 'UTF-8'
+
+
+    def getFileName(self) -> str:
+        return self._targetFile
+
+
+    def loadListFromFile(self, fileName:str=None) -> list:
+        # 혹시나 setFile 안하고 바로 해당 함수 들어갈 경우를 대비한 조건문
+        if fileName:
+            self.setFile(fileName)
+
+        # 얘가 setFile 이후 정상 루트
+        else:
+            fileName = self._targetFile
+
+        resList:list = []
+
+        if not fileName:
+            return None
+
+        if self._originEncoding == 'ASCII':
+            encFormat = 'mbcs'
+        else:
+            encFormat = self._originEncoding
+            
+        with open(fileName, 'r', encoding=encFormat) as f:
+            for eachLine in f:
+                eachLine = eachLine.strip('\n')
+                resList.append(eachLine)
+
+        return resList
+
+
+    def getEncodingFormat(self, fileName:str=None) -> str:
+        if fileName and os.path.isfile(fileName) is False:
+            raise Exception(f'[TextLoader/getEncodingFormat] \'{fileName}\' is not exist.')
+        return self._getFileEncoding(fileName)
+
+
+# Package Function
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+def setTextLoader(fileName:str=None) -> TextLoader:
+    return TextLoader(fileName)
+
+
+def loadListFromFile(fileName:str) -> list:
+    return setTextLoader(fileName).loadListFromFile()
+
+
+def getEncodingFormat(fileName:str) -> str:
+    return setTextLoader(fileName).getEncodingFormat()
---
+++ UnitTest_TextLoader.py
@@ -0,0 +1,431 @@
+import  unittest
+from    TextLoader  import *
+
+
+def writeFileFromList(fileName:str, tList:list, encodingFormat:str):
+    with open(fileName, 'w', encoding=encodingFormat) as wf:
+        if tList:
+            for line in tList:
+                wf.write(f'{line}\n')
+
+
+TEST_TXT_LIST   = ['123', 'abc', 'A1B2C3', '!@#$%^&', 'ㄱㄴㄷ', '가나다라마바사']
+EMPTY_TXT_LIST  = []
+
+UTF8    = 'UTF-8'
+ASCII   = 'ASCII'
+
+file_UTF8_name          = './UnitTest/UnitTest_UTF8.txt'
+file_ASCII_name         = './UnitTest/UnitTest_ASCII.txt'
+file_empty_UTF8_name    = './UnitTest/UnitTest_Empty_UTF8.txt'
+file_empty_ASCII_name   = './UnitTest/UnitTest_Empty_ASCII.txt'
+wrong_file_name         = './UnitTest/noneExistFile.txt'
+UNITTEST_DIR            = './UnitTest'
+
+# TestCase
+class CustomTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """ Before Start UnitTest"""
+        if os.path.isdir(UNITTEST_DIR) is False:
+            os.makedirs(UNITTEST_DIR, exist_ok=True)
+
+        writeFileFromList(file_UTF8_name,           TEST_TXT_LIST,  'utf-8')
+        writeFileFromList(file_ASCII_name,          TEST_TXT_LIST,  'mbcs' )
+        writeFileFromList(file_empty_UTF8_name,     EMPTY_TXT_LIST, 'utf-8')
+        writeFileFromList(file_empty_ASCII_name,    EMPTY_TXT_LIST, 'mbcs' )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """ After Finish Unit Test """
+        if os.path.isfile(file_UTF8_name):
+            os.remove(file_UTF8_name)
+
+        if os.path.isfile(file_ASCII_name):
+            os.remove(file_ASCII_name)
+
+        if os.path.isfile(file_empty_UTF8_name):
+            os.remove(file_empty_UTF8_name)
+
+        if os.path.isfile(file_empty_ASCII_name):
+            os.remove(file_empty_ASCII_name)
+
+
+    # TEST
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+    """
+        I. 파일 등록 방법
+            1. TextLoader 클래스 생성 시 인자로 즉시 넘김
+            2. setFile 사용
+            3. 각 함수에 직접 입력
+        
+        II. 파일 유형
+            1. 유효한 파일 : UTF-8
+            2. 유효한 파일 : ASCII
+            3. 빈 파일 : UTF-8
+            4. 빈 파일 : ASCII
+            5. 존재하지 않는 파일
+
+        III. get 함수
+            1. fileName
+            2. encoding ( private, public method, overloading func )
+            3. textList ( public method, overloading func )
+
+        IV. 기타 체크 항목
+
+    """
+
+    # File Register + get FileName Test
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+    #01 
+    # I.1 + II.1 + III.1) TextLoader 생성자 옳은 파일 이름 입력
+    def test_set_right_filename_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertIsNotNone(tL.getFileName())
+
+    #02
+    # I.1 + III.1) TextLoader 생성자 파일 이름 입력 안함
+    def test_not_set_filename_when_class_create(self):
+        tL = TextLoader()
+        self.assertIsNone(tL.getFileName())
+
+    #03
+    # I.1 + II.5) TextLoader 생성자 잘못된 파일 이름 입력
+    def test_set_wrong_filename_when_class_create(self):
+        self.assertRaises(Exception, TextLoader, wrong_file_name)
+
+    #04
+    # I.1 + II.1 + III.1) TextLoader 생성 시 파일 이름 입력했을 때, getFileName() 
+    def test_get_right_filename_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertEqual(tL.getFileName(), file_UTF8_name)
+
+    # I.1 + III.1) TextLoader 생성 시 파일 이름 입력 안했을 때, getFileName()
+    # Case02 와 동일
+
+    #05
+    # I.2 + II.1 + III.1) TextLoader 생성 이후에 옳은 파일 이름 setFile()
+    def test_set_right_filename_when_setFile_after_class_create(self):
+        tL = TextLoader()
+        tL.setFile(file_UTF8_name)
+        self.assertIsNotNone(tL.getFileName())
+
+    #06
+    # I.2 + II.5) TextLoader 생성 이후에 잘못된 파일 이름 setFile()
+    def test_set_wrong_filename_when_setFile_after_class_create(self):
+        tL = TextLoader()
+        self.assertRaises(Exception, tL.setFile, wrong_file_name)
+
+    #07
+    # I.2 + II.2 + III.1) TextLoader 생성 이후에 여러 번 setFile(), 모두 옳은 파일
+    def test_setFile_multiple_all_right(self):
+        tL = TextLoader()
+        tL.setFile(file_UTF8_name)
+        tL.setFile(file_empty_UTF8_name)
+        tL.setFile(file_ASCII_name)
+        self.assertEqual(tL.getFileName(), file_ASCII_name)
+
+    #08
+    # I.2 + II.5) TextLoader 생성 이후에 여러 번 setFile(), 중간 잘못된 파일
+    def test_setFile_multiple_one_wrong(self):
+        tL = TextLoader()
+        tL.setFile(file_UTF8_name)
+        tL.setFile(file_empty_UTF8_name)
+        self.assertRaises(Exception, tL.setFile, wrong_file_name)
+
+    #09
+    # I.2 + II.1 + III.1) TextLoader 생성자 옳은 파일 이름 입력 이후 다른 파일 setFile
+    def test_set_right_filename_when_setFile_already_set_filename(self):
+        tL = TextLoader(file_ASCII_name)
+        tL.setFile(file_UTF8_name)
+        self.assertEqual(tL.getFileName(), file_UTF8_name)
+
+
+    # Get Encoding Test :: private Function : _getFileEncoding()
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+    #10
+    # I.1 + II.1 + III.2) TextLoader 생성자 옳은 파일 이름 입력 + private _getFileEncoding()
+    def test_get_encoding_privFunc_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertEqual(tL._getFileEncoding(), UTF8)
+
+    #11
+    # I.1 + II.2 + III.2) TextLoader 생성자 옳은 파일 이름 입력 + private _getFileEncoding(other file)
+    def test_get_newReg_encoding_privFunc_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertEqual(tL._getFileEncoding(file_ASCII_name), ASCII)
+
+    #12
+    # I.1 + III.2) TextLoader 생성자 파일 이름 입력 안함 + private _getFileEncoding()
+    def test_get_encoding_privFunc_when_not_set_filename_ever(self):
+        tL = TextLoader()
+        self.assertRaises(Exception, tL._getFileEncoding)
+
+    #13
+    # I.1 + III.2) TextLoader 생성자 파일 이름 입력 안함 + private _getFileEncoding(file)
+    def test_get_newReg_encoding_privFunc_when_not_set_class_create(self):
+        tL = TextLoader()
+        self.assertEqual(tL._getFileEncoding(file_ASCII_name), ASCII)
+
+    #14
+    # I.1 + II.5 + III.2) TextLoader 생성자 옳은 파일 이름 입력 + private _getFileEncoding(잘못된 파일)
+    def test_get_newWrongReg_encoding_privFunc_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertRaises(Exception, tL._getFileEncoding, wrong_file_name)
+
+    # I.1 + II.1 + III.2) TextLoader 생성 시 파일 이름 입력했을 때, _getFileEncoding() / UTF8
+    # Case10 과 동일
+
+    #15
+    # I.1 + II.1 + III.2) TextLoader 생성 시 파일 이름 입력 안 했을 때, _getFileEncoding(file) / UTF8
+    def test_get_right_encoding_UTF8_privFunc_when_not_set_class_create(self):
+        tL = TextLoader()
+        self.assertEqual(tL._getFileEncoding(file_UTF8_name), UTF8)
+
+    #16
+    # I.2 + II.1 + III.2) TextLoader 생성 이후에 옳은 파일 이름 setFile(), _getFileEncoding() / UTF8
+    def test_get_right_encoding_UTF8_privFunc_when_setFile_after_class_create(self):
+        tL = TextLoader()
+        tL.setFile(file_UTF8_name)
+        self.assertEqual(tL._getFileEncoding(), UTF8)
+
+    #17
+    # I.2 + II.1 + III.2) TextLoader 생성 이후에 setFile() 파일 변경, _getFileEncoding() / UTF8
+    def test_get_right_encoding_UTF8_privFunc_when_change_setFile(self):
+        tL = TextLoader(file_ASCII_name)
+        tL.setFile(file_UTF8_name)
+        self.assertEqual(tL._getFileEncoding(), UTF8)
+
+    #18
+    # I.3 + II.1 + III.2)  TextLoader 생성 이후에 함수 자체에서 _getFileEncoding(file) / UTF8
+    def test_get_right_encoding_UTF8_privFunc_when_set_funcSelf(self):
+        tL = TextLoader(file_ASCII_name)
+        self.assertEqual(tL._getFileEncoding(file_UTF8_name), UTF8)
+
+    #19
+    # I.1 + II.2 + III.2) TextLoader 생성 시 파일 이름 입력했을 때, _getFileEncoding() / ASCII
+    def test_get_right_encoding_ASCII_privFunc_when_class_create(self):
+        tL = TextLoader(file_ASCII_name)
+        self.assertEqual(tL._getFileEncoding(), ASCII)
+
+    # I.1 + II.2 + III.2) TextLoader 생성 시 파일 이름 입력 안 했을 때, _getFileEncoding(file) / ASCII
+    # Case13 과 동일
+
+    #20
+    # I.2 + II.2 + III.2) TextLoader 생성 이후에 옳은 파일 이름 setFile(), _getFileEncoding() / ASCII
+    def test_get_right_encoding_ASCII_privFunc_when_setFile_after_class_create(self):
+        tL = TextLoader()
+        tL.setFile(file_ASCII_name)
+        self.assertEqual(tL._getFileEncoding(), ASCII)
+
+    #21
+    # I.2 + II.2 + III.2) TextLoader 생성 이후에 setFile() 파일 변경, _getFileEncoding() / ASCII
+    def test_get_right_encoding_ASCII_privFunc_when_change_setFile(self):
+        tL = TextLoader(file_UTF8_name)
+        tL.setFile(file_ASCII_name)
+        self.assertEqual(tL._getFileEncoding(), ASCII)
+
+    # I.3 + II.2 + III.2)  TextLoader 인자 있게 생성 이후에 함수 자체에서 _getFileEncoding(file) / ASCII
+    # Case11 과 동일
+
+    # I.3 + II.2 + III.2)  TextLoader 인자 없이 생성 이후에 함수 자체에서 _getFileEncoding(file) / ASCII
+    # Case13 과 동일
+
+    #22
+    # I.1 + II.3 + III.2) 빈 파일을 열 때 / UTF8
+    def test_get_encoding_UTF8_privFunc_when_open_empty_file(self):
+        tL = TextLoader(file_empty_UTF8_name)
+        self.assertEqual(tL._getFileEncoding(), UTF8)
+
+    #23
+    # I.1 + II.4 + III.2) 빈 파일은 ASCII -> UTF8로 자동 변환됨
+    def test_get_encoding_ASCII_privFunc_when_open_empty_file(self):
+        tL = TextLoader(file_empty_ASCII_name)
+        self.assertEqual(tL._getFileEncoding(), UTF8)
+
+
+    # Get Encoding Test :: method : getEncodingFormat()
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+    #24
+    # I.1 + II.1 + III.2) TextLoader 생성자 옳은 파일 이름 입력 + method getEncodingFormat()
+    def test_get_encoding_method_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertEqual(tL.getEncodingFormat(), UTF8)
+
+    #25
+    # I.1 + II.2 + III.2) TextLoader 생성자 옳은 파일 이름 입력 + method getEncodingFormat(other file)
+    def test_get_newReg_encoding_method_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertEqual(tL.getEncodingFormat(file_ASCII_name), ASCII)
+
+    #26
+    # I.1 + II.5 + III.2) TextLoader 생성자 파일 이름 입력 안함 + method getEncodingFormat()
+    def test_get_encoding_method_when_not_set_filename_ever(self):
+        tL = TextLoader()
+        self.assertRaises(Exception, tL.getEncodingFormat)
+
+    #27
+    # I.1 + II.2 + III.2) TextLoader 생성자 파일 이름 입력 안함 + method getEncodingFormat(file)
+    def test_get_newReg_encoding_method_when_not_set_class_create(self):
+        tL = TextLoader()
+        self.assertEqual(tL.getEncodingFormat(file_ASCII_name), ASCII)
+
+    #28
+    # I.1 + II.5 + III.2) TextLoader 생성자 옳은 파일 이름 입력 + method getEncodingFormat(잘못된 파일)
+    def test_get_newWrongReg_encoding_method_when_class_create(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertRaises(Exception, tL.getEncodingFormat, wrong_file_name)
+
+    # I.1 + II.1 + III.2) TextLoader 생성 시 파일 이름 입력했을 때, getEncodingFormat() / UTF8
+    # Case24 과 동일
+
+    #29
+    # I.1 + II.1 + III.2) TextLoader 생성 시 파일 이름 입력 안 했을 때, getEncodingFormat(file) / UTF8
+    def test_get_right_encoding_UTF8_method_when_not_set_class_create(self):
+        tL = TextLoader()
+        self.assertEqual(tL.getEncodingFormat(file_UTF8_name), UTF8)
+
+    #30
+    # I.2 + II.1 + III.2) TextLoader 생성 이후에 옳은 파일 이름 setFile(), getEncodingFormat() / UTF8
+    def test_get_right_encoding_UTF8_method_when_setFile_after_class_create(self):
+        tL = TextLoader()
+        tL.setFile(file_UTF8_name)
+        self.assertEqual(tL.getEncodingFormat(), UTF8)
+
+    #31
+    # I.2 + II.1 + III.2) TextLoader 생성 이후에 setFile() 파일 변경, getEncodingFormat() / UTF8
+    def test_get_right_encoding_UTF8_method_when_change_setFile(self):
+        tL = TextLoader(file_ASCII_name)
+        tL.setFile(file_UTF8_name)
+        self.assertEqual(tL.getEncodingFormat(), UTF8)
+
+    #32
+    # I.3 + II.1 + III.2)  TextLoader 생성 이후에 함수 자체에서 getEncodingFormat(file) / UTF8
+    def test_get_right_encoding_UTF8_method_when_set_funcSelf(self):
+        tL = TextLoader(file_ASCII_name)
+        self.assertEqual(tL.getEncodingFormat(file_UTF8_name), UTF8)
+
+    #33
+    # I.1 + II.2 + III.2) TextLoader 생성 시 파일 이름 입력했을 때, getEncodingFormat() / ASCII
+    def test_get_right_encoding_ASCII_method_when_class_create(self):
+        tL = TextLoader(file_ASCII_name)
+        self.assertEqual(tL.getEncodingFormat(), ASCII)
+
+    # I.1 + II.2 + III.2) TextLoader 생성 시 파일 이름 입력 안 했을 때, getEncodingFormat(file) / ASCII
+    # Case27 과 동일
+
+    #34
+    # I.2 + II.2 + III.2) TextLoader 생성 이후에 옳은 파일 이름 setFile(), getEncodingFormat() / ASCII
+    def test_get_right_encoding_ASCII_method_when_setFile_after_class_create(self):
+        tL = TextLoader()
+        tL.setFile(file_ASCII_name)
+        self.assertEqual(tL.getEncodingFormat(), ASCII)
+
+    #35
+    # I.2 + II.2 + III.2) TextLoader 생성 이후에 setFile() 파일 변경, getEncodingFormat() / ASCII
+    def test_get_right_encoding_ASCII_method_when_change_setFile(self):
+        tL = TextLoader(file_UTF8_name)
+        tL.setFile(file_ASCII_name)
+        self.assertEqual(tL.getEncodingFormat(), ASCII)
+
+    # I.3 + III.2)  TextLoader 인자 있게 생성 이후에 함수 자체에서 getEncodingFormat(file) / ASCII
+    # Case25 과 동일
+
+    # I.3 + III.2)  TextLoader 인자 없이 생성 이후에 함수 자체에서 getEncodingFormat(file) / ASCII
+    # Case27 과 동일
+
+    #36
+    # 빈 파일을 열 때 / UTF8
+    def test_get_encoding_UTF8_method_when_open_empty_file(self):
+        tL = TextLoader(file_empty_UTF8_name)
+        self.assertEqual(tL.getEncodingFormat(), UTF8)
+
+    #37
+    # 빈 파일은 ASCII -> UTF8로 자동 변환됨
+    def test_get_encoding_ASCII_method_when_open_empty_file(self):
+        tL = TextLoader(file_empty_ASCII_name)
+        self.assertEqual(tL.getEncodingFormat(), UTF8)
+
+
+    # Get Encoding Test :: overloading Func : getEncodingFormat
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* #
+    #38
+    def test_get_encoding_UTF8_overloading(self):
+        self.assertEqual(getEncodingFormat(file_UTF8_name), UTF8)
+
+    def test_get_encoding_ASCII_overloading(self):
+        self.assertEqual(getEncodingFormat(file_ASCII_name), ASCII)
+
+    # 빈 파일을 열 때 / UTF8
+    def test_get_encoding_UTF8_overloading_when_open_empty_file(self):
+        self.assertEqual(getEncodingFormat(file_empty_UTF8_name), UTF8)
+
+    # 빈 파일은 ASCII -> UTF8로 자동 변환됨
+    def test_get_encoding_ASCII_overloading_when_open_empty_file(self):
+        self.assertEqual(getEncodingFormat(file_empty_ASCII_name), UTF8)
+
+    def test_get_encoding_when_wrong_file_register(self):
+        self.assertRaises(Exception, getEncodingFormat, wrong_file_name)
+
+    def test_isInstance_setTextLoader_none_set_filename(self):
+        self.assertIsInstance(setTextLoader(), TextLoader)
+
+    def test_isInstance_setTextLoader_set_filename(self):
+        self.assertIsInstance(setTextLoader(file_UTF8_name), TextLoader)
+
+    def test_get_filename_after_setTextLoader_set_filename(self):
+        self.assertEqual(setTextLoader(file_UTF8_name).getFileName(), file_UTF8_name)
+
+    def test_get_filename_after_setTextLoader_not_set_filename(self):
+        self.assertIsNone(setTextLoader().getFileName())
+
+    def test_get_encoding_after_setTextLoader_set_filename(self):
+        self.assertEqual(setTextLoader(file_UTF8_name).getEncodingFormat(), UTF8)
+
+    def test_get_encoding_after_setTextLoader_not_set_filename(self):
+        self.assertRaises(Exception, setTextLoader().getEncodingFormat)
+
+    def test_get_list_encoding_UTF8_in_class_when_set_filename_init(self):
+        tL = TextLoader(file_UTF8_name)
+        self.assertEqual(tL.loadListFromFile(), TEST_TXT_LIST)
+
+    def test_get_list_encoding_ASCII_in_class_when_set_filename_init(self):
+        tL = TextLoader(file_ASCII_name)
+        self.assertEqual(tL.loadListFromFile(), TEST_TXT_LIST)
+
+    def test_get_list_encoding_UTF8_in_class_when_set_filename_init_empty(self):
+        tL = TextLoader(file_empty_UTF8_name)
+        self.assertEqual(tL.loadListFromFile(), EMPTY_TXT_LIST)
+
+    def test_get_list_encoding_ASCII_in_class_when_set_filename_init_empty(self):
+        tL = TextLoader(file_empty_ASCII_name)
+        self.assertEqual(tL.loadListFromFile(), EMPTY_TXT_LIST)
+
+    def test_get_right_list_UTF8_when_not_set_file_init(self):
+        self.assertEqual(TextLoader().loadListFromFile(file_UTF8_name), TEST_TXT_LIST)
+
+    def test_get_right_list_ASCII_when_not_set_file_init(self):
+        self.assertEqual(TextLoader().loadListFromFile(file_ASCII_name), TEST_TXT_LIST)
+
+    def test_get_none_list_when_TextLoader_not_set_filename_ever(self):
+        self.assertIsNone(TextLoader().loadListFromFile())
+
+    def test_get_raise_when_loadListFromFile_not_set_filename_ever(self):
+        self.assertRaises(Exception, loadListFromFile)
+
+    def test_get_raise_getEncodingFormat_set_none_param(self):
+        self.assertRaises(Exception, getEncodingFormat)
+
+    def test_get_right_list_UTF8_overloading(self):
+        self.assertEqual(loadListFromFile(file_UTF8_name), TEST_TXT_LIST)
+
+    def test_get_right_list_ASCII_overloading(self):
+        self.assertEqual(loadListFromFile(file_ASCII_name), TEST_TXT_LIST)
+
+    def test_get_raises_loadListFromFile_when_set_wrong_file(self):
+        self.assertRaises(Exception, loadListFromFile, wrong_file_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
